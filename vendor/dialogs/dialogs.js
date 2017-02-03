6359c4518a0d959bd9bd12a8e5b81b35e4778b5c
Ali Zahid <ali.zahid@live.com>
2017-02-03 10:23:43 +0400

    Uglify does not like ES6

--- vendor/dialogs/dialogs.js
+++ vendor/dialogs/dialogs.js
@@ -1,90 +1,148 @@
-class Dialog {
-	static alert(message) {
-		return this.dialog('alert', message, ['Okay'])
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function(obj) {
+	return typeof obj;
+} : function(obj) {
+	return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+};
+
+var _createClass = function() {
+	function defineProperties(target, props) {
+		for (var i = 0; i < props.length; i++) {
+			var descriptor = props[i];
+			descriptor.enumerable = descriptor.enumerable || false;
+			descriptor.configurable = true;
+			if ("value" in descriptor) descriptor.writable = true;
+			Object.defineProperty(target, descriptor.key, descriptor);
+		}
 	}
+	return function(Constructor, protoProps, staticProps) {
+		if (protoProps) defineProperties(Constructor.prototype, protoProps);
+		if (staticProps) defineProperties(Constructor, staticProps);
+		return Constructor;
+	};
+}();
 
-	static confirm(message, buttons = ['Yes', 'No']) {
-		return this.dialog('confirm', message, buttons)
-	}
-
-	static prompt(message, value, buttons = ['Okay', 'Cancel']) {
-		return this.dialog('prompt', message, buttons, value)
-	}
-
-	static multiPrompt(message, fields = {}, buttons = ['Okay', 'Cancel']) {
-		return this.dialog('prompt', message, buttons, fields)
-	}
-
-	static dialog(type, message, buttons = [], promptValue) {
-		return new Promise(resolve => {
-			let overlay = $('<div>').addClass('overlay')
-			let dialog = $('<div>')
-
-			dialog.addClass('dialog').addClass(type)
-
-			if (typeof promptValue === 'object') {
-				$('<h3>').text(message).appendTo(dialog)
-			} else {
-				$('<p>').text(message).appendTo(dialog)
-			}
-
-			if (type === 'prompt') {
-				if (typeof promptValue === 'object') {
-					Object.keys(promptValue).forEach(key => {
-						let label = promptValue[key].label
-						let value = promptValue[key].value
-
-						$('<input>').prop('type', 'text').prop('placeholder', label).data('key', key).val(value).appendTo(dialog)
-					})
-				} else {
-					$('<input>').prop('type', 'text').val(promptValue).appendTo(dialog)
-
-					dialog.wrapInner('<label>')
-				}
-			}
-
-			let footer = $('<footer>')
-
-			buttons.forEach((button, index) => {
-				$('<button>').text(button).on('click', () => {
-					if (index === 0) {
-						if (type === 'prompt') {
-							if (typeof promptValue === 'object') {
-								let data = dialog.find('input').each((index, input) => {
-									let key = $(input).data('key')
-									let value = $(input).val().trim()
-
-									promptValue[key] = value
-								})
-
-								resolve(promptValue)
-							} else {
-								let data = dialog.find('input').val().trim()
-
-								if (data.length > 0) {
-									resolve(data)
-								}
-							}
-						} else {
-							resolve()
-						}
-					}
-
-					overlay.fadeOut('fast', () => overlay.remove())
-				}).appendTo(footer)
-			})
-
-			dialog.append(footer)
-
-			overlay.append(dialog).on('click', e => {
-				if ($(e.target).hasClass('overlay')) {
-					overlay.fadeOut('fast', () => overlay.remove())
-				}
-			}).hide().appendTo('body').fadeIn('fast', () => {
-				if (type === 'prompt') {
-					dialog.find('input').get(0).focus()
-				}
-			})
-		})
+function _classCallCheck(instance, Constructor) {
+	if (!(instance instanceof Constructor)) {
+		throw new TypeError("Cannot call a class as a function");
 	}
 }
+
+var Dialog = function() {
+	function Dialog() {
+		_classCallCheck(this, Dialog);
+	}
+
+	_createClass(Dialog, null, [{
+		key: 'alert',
+		value: function alert(message) {
+			return this.dialog('alert', message, ['Okay']);
+		}
+	}, {
+		key: 'confirm',
+		value: function confirm(message) {
+			var buttons = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : ['Yes', 'No'];
+
+			return this.dialog('confirm', message, buttons);
+		}
+	}, {
+		key: 'prompt',
+		value: function prompt(message, value) {
+			var buttons = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : ['Okay', 'Cancel'];
+
+			return this.dialog('prompt', message, buttons, value);
+		}
+	}, {
+		key: 'multiPrompt',
+		value: function multiPrompt(message) {
+			var fields = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+			var buttons = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : ['Okay', 'Cancel'];
+
+			return this.dialog('prompt', message, buttons, fields);
+		}
+	}, {
+		key: 'dialog',
+		value: function dialog(type, message) {
+			var buttons = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+			var promptValue = arguments[3];
+
+			return new Promise(function(resolve) {
+				var overlay = $('<div>').addClass('overlay');
+				var dialog = $('<div>');
+
+				dialog.addClass('dialog').addClass(type);
+
+				if ((typeof promptValue === 'undefined' ? 'undefined' : _typeof(promptValue)) === 'object') {
+					$('<h3>').text(message).appendTo(dialog);
+				} else {
+					$('<p>').text(message).appendTo(dialog);
+				}
+
+				if (type === 'prompt') {
+					if ((typeof promptValue === 'undefined' ? 'undefined' : _typeof(promptValue)) === 'object') {
+						Object.keys(promptValue).forEach(function(key) {
+							var label = promptValue[key].label;
+							var value = promptValue[key].value;
+
+							$('<input>').prop('type', 'text').prop('placeholder', label).data('key', key).val(value).appendTo(dialog);
+						});
+					} else {
+						$('<input>').prop('type', 'text').val(promptValue).appendTo(dialog);
+
+						dialog.wrapInner('<label>');
+					}
+				}
+
+				var footer = $('<footer>');
+
+				buttons.forEach(function(button, index) {
+					$('<button>').text(button).on('click', function() {
+						if (index === 0) {
+							if (type === 'prompt') {
+								if ((typeof promptValue === 'undefined' ? 'undefined' : _typeof(promptValue)) === 'object') {
+									var data = dialog.find('input').each(function(index, input) {
+										var key = $(input).data('key');
+										var value = $(input).val().trim();
+
+										promptValue[key] = value;
+									});
+
+									resolve(promptValue);
+								} else {
+									var _data = dialog.find('input').val().trim();
+
+									if (_data.length > 0) {
+										resolve(_data);
+									}
+								}
+							} else {
+								resolve();
+							}
+						}
+
+						overlay.fadeOut('fast', function() {
+							return overlay.remove();
+						});
+					}).appendTo(footer);
+				});
+
+				dialog.append(footer);
+
+				overlay.append(dialog).on('click', function(e) {
+					if ($(e.target).hasClass('overlay')) {
+						overlay.fadeOut('fast', function() {
+							return overlay.remove();
+						});
+					}
+				}).hide().appendTo('body').fadeIn('fast', function() {
+					if (type === 'prompt') {
+						dialog.find('input').get(0).focus();
+					}
+				});
+			});
+		}
+	}]);
+
+	return Dialog;
+}();
