68f77ec454407591ea7c12cf8c50b2c3c10eecba
Ali Zahid <ali.zahid@live.com>
2017-03-02 03:05:29 +0500

    Models updated

--- app/models/tag.js
+++ app/models/tag.js
@@ -3,5 +3,6 @@ import DS from 'ember-data'
 export default DS.Model.extend({
 	name: DS.attr(),
 	type: DS.attr(),
-	order: DS.attr()
+	order: DS.attr(),
+	related: DS.attr()
 })
--- app/models/user.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data'
-
-export default DS.Model.extend({
-	reported: DS.attr(),
-	notifications: DS.attr(),
-	token: DS.attr(),
-	device: DS.attr()
-})
