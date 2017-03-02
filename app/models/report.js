cee26b41ff6875c85d5367192b3b2e41a8535675
Ali Zahid <ali.zahid@live.com>
2017-03-02 11:20:59 +0500

    Scene added

--- app/models/report.js
+++ app/models/report.js
@@ -2,6 +2,6 @@ import DS from 'ember-data'
 
 export default DS.Model.extend({
 	user: DS.attr(),
-	post: DS.attr(),
+	post: DS.belongsTo('post'),
 	details: DS.attr()
 })
--- app/routes/reports.js
+++ app/routes/reports.js
@@ -1,5 +1,14 @@
 import Ember from 'ember'
+import InfinityRoute from 'ember-infinity/mixins/route'
 
-export default Ember.Route.extend({
-	titleToken: `Reports`
+export default Ember.Route.extend(InfinityRoute, {
+	titleToken: `Reports`,
+
+	totalPagesParam: 'meta.total',
+
+	model() {
+		return this.infinityModel('report', {
+			startingPage: 0
+		})
+	}
 })
