134be5d0b224c76dea5034cbd0456cfe84e530cb
Ali Zahid <ali.zahid@live.com>
2017-03-02 07:24:35 +0500

    Message components added

--- app/components/error-message.js
+++ app/components/error-message.js
@@ -4,14 +4,22 @@ const component = Ember.Component.extend({
 	classNames: ['error'],
 
 	messageChanged: Ember.observer('error', function() {
-		let error = this.get('error.errors.firstObject.detail')
+		if (this.error) {
+			let error = this.get('error.errors.firstObject.detail')
 
-		if (error && error.message) {
-			this.$().text(error.message)
-		} else if (error) {
-			this.$().text(`Something went wrong`)
+			if (error && error.message) {
+				this.$().addClass('visible').text(error.message)
+			} else if (error) {
+				this.$().addClass('visible').text(`Something went wrong`)
+			}
 		}
-	})
+	}),
+
+	click() {
+		this.$().removeClass('visible').text('')
+
+		this.set('error', null)
+	}
 })
 
 component.reopenClass({
---
+++ app/components/success-message.js
@@ -0,0 +1,25 @@
+import Ember from 'ember'
+
+const component = Ember.Component.extend({
+	classNames: ['success'],
+
+	delay: 5000,
+
+	messageChanged: Ember.observer('message', function() {
+		if (this.message) {
+			this.$().addClass('visible').text(this.message)
+
+			Ember.run.later(() => {
+				this.$().removeClass('visible').text('')
+
+				this.set('message', null)
+			}, this.delay)
+		}
+	})
+})
+
+component.reopenClass({
+	positionalParams: ['message']
+})
+
+export default component
